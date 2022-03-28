ea5137e6ef0d5442c2f5ca18a0269f695ef2f4fa
p2021inf1 <p2021inf1@jaumebalmes.net>
2022-03-28 18:13:17 +0200

    second

---
+++ src/git_test2/java2.java
@@ -0,0 +1,13 @@
+package git_test2;
+
+public class java2 {
+
+	public static void main(String[] args) {
+		int i=1;
+		int j=2;
+		int k=i+j;
+		
+		System.out.print("el resultat és: "+k);
+	}
+
+}
